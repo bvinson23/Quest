c039693830014de87d6d0953d434eafcb1341680
bvinson23 <brandonvinson23@gmail.com>
2021-05-27 11:38:14 -0500

    created Robe

---
+++ Robe.cs
@@ -0,0 +1,13 @@
+using System.Collections.Generic;
+
+namespace Quest
+{
+    public class Robe
+    {
+        public List<string> Colors = new List<string>
+        {
+            
+        };
+        public int Length { get; set; }
+    }
+}
